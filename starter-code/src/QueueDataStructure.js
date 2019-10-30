8b10ece38ef94b81f73013d539ae6fadbe588d5a
Jaxaver <jaxaver@LAPTOP-HG48MLL7.localdomain>
2019-10-30 20:11:35 +0100

    internal logic almost done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,32 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+
+    constructor() {
+        this.queueControl = []
+        this.MAX_SIZE = 5
+    }
+
+    isEmpty() {
+        return true
+    }
+
+    canEnqueue() {
+        if (this.queueControl.length == 0) {
+            return false
+        } else {
+            return true
+        }
+    }
+
+    enqueue(n) {
+        this.queueControl.unshift(n)
+        return this.queueControl
+    }
+
+    dequeue() {
+        
+        
+    }
+
 }
+
+const queueDataStructure = new QueueDataStructure
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,31 @@
-function StackDataStructure () {
+class StackDataStructure {
+
+    constructor() {
+        this.stackControl = []
+        this.MAX_SIZE = 5
+    }
+
+    isEmpty() {
+       
+        return this.stackControl.length < 1
+        
+    }
+
+    canPush() {
+        return this.stackControl.length < this.MAX_SIZE
+    }
+
+    push(n) {
+        
+        this.stackControl.push(n)
+        
+        return this.stackControl
+    }
+
+    pop() {
+
+    }
+
 }
+
+const stackDataStructure = new StackDataStructure
